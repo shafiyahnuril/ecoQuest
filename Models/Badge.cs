b531a483c000f16ae73cfcbb8708994da589d2bc
aurelia-mutiah <aureliamutiahraudyatuzzahra@mail.ugm.ac.id>
2026-09-12 22:05:35 +0700

    Add Badge, UserBadge, WeatherCache model classes (Modul 3)

---
+++ Models/Badge.cs
@@ -0,0 +1,33 @@
+﻿namespace EcoQuest.Models
+{
+    public enum CriteriaType
+    {
+        ActivityCount,
+        PointsThreshold,
+        StreakDays,
+        CO2SavedThreshold
+    }
+
+    public class Badge
+    {
+        private int _badgeId;
+        private string _name;
+        private string _description;
+        private CriteriaType _criteriaType;
+
+        public int BadgeId { get => _badgeId; set => _badgeId = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Description { get => _description; set => _description = value; }
+        public CriteriaType CriteriaType { get => _criteriaType; set => _criteriaType = value; }
+
+        public Badge() { }
+
+        public Badge(int badgeId, string name, string description, CriteriaType criteriaType)
+        {
+            BadgeId = badgeId;
+            Name = name;
+            Description = description;
+            CriteriaType = criteriaType;
+        }
+    }
+}
---
+++ Models/UserBadge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EcoQuest.Models
+{
+    public class UserBadge
+    {
+        private int _badgeId;
+        private Guid _userId;
+        private DateTime _earnedAt;
+
+        public int BadgeId { get => _badgeId; set => _badgeId = value; }
+        public Guid UserId { get => _userId; set => _userId = value; }
+        public DateTime EarnedAt { get => _earnedAt; set => _earnedAt = value; }
+
+        public UserBadge() { }
+
+        public UserBadge(int badgeId, Guid userId, DateTime earnedAt)
+        {
+            BadgeId = badgeId;
+            UserId = userId;
+            EarnedAt = earnedAt;
+        }
+    }
+}
